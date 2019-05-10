e19381502810ed17e99d546983bf622c1b436db8
Arthur Ashman <arthurashman@gmail.com>
2019-05-10 09:56:20 +0100

    Add tests for item class
    Covers to_s method

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,5 +2,10 @@ require 'item'
 require 'gilded_rose'
 
 describe Item do
-  
+  describe '#to_s' do
+    it 'formats item output' do
+      mongoose_elixir = Item.new(name='Elixir of the Mongoose', sell_in=5, quality=7)
+      expect(mongoose_elixir.to_s).to eq('Elixir of the Mongoose, 5, 7')
+    end
+  end
 end
